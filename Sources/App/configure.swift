eec5b13c8c707dd0ad2328a7336fedc6e97dcaff
Jonathan Green <Jgreen@Stephanies-MBP.attlocal.net>
2018-10-07 10:55:16 -0400

    WIP

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,9 +10,20 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-
+    
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
+    
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+    middlewares.use(corsMiddleware)
+    middlewares.use(ErrorMiddleware.self)
+    services.register(middlewares)
+    
     /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
